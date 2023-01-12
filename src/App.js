1ccbdb7ff77f84320e07c864d47dcba3b35563fb
Yeonho Cho <nanyeonho@gmail.com>
2023-01-12 12:41:50 +0900

    #2. useEffect 02
    Deps : useEffect renders when special elements change.
    
    useEffect( codes that u want to render only once , codes in this useEffect will render if this element changes)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onKeyChange = (event) => setKeyword(event.target.value);
+
   const onClick = () => setValue((prev) => prev + 1);
-  useEffect(() => console.log("call the API..."), []);
-  console.log("i run all the time");
+  useEffect(() => console.log("i run only once"), []);
+
+  useEffect(() => {
+    if (keyword != "") {
+      console.log("i run when 'keyword' changes");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    if (counter != 0) {
+      console.log("i run when 'counter' changes");
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    if (counter != 0 || keyword != "") {
+      console.log("i run when 'counter' and 'keyword' change");
+    }
+  }, [counter, keyword]);
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Search here..."
+        onChange={onKeyChange}
+      ></input>
+      <h1>{keyword}</h1>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
